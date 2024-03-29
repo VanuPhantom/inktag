1bf5380272805cba7e8f50087ab28fe9ce5c8cde
Tess Ellenoir Duursma <tess@vanu.dev>
2024-03-29 21:42:48 +0100

    Start using platformio

--- shell.nix
+++ shell.nix
@@ -1,25 +1,14 @@
 # A standalone shell definition that downloads and uses packages from `nixpkgs-esp-dev` automatically.
 
 let
-  nixpkgs-esp-dev = builtins.fetchGit {
-    url = "https://github.com/mirrexagon/nixpkgs-esp-dev.git";
-
-    # Optionally pin to a specific commit of `nixpkgs-esp-dev`.
-    # rev = "<commit hash>";
-  };
-
-  pkgs = import <nixpkgs> { overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ]; };
+  pkgs = import <nixpkgs> { };
 in
-pkgs.mkShell {
-  name = "esp-project";
 
-  buildInputs = with pkgs; [
-    esp-idf-full
-  ];
+pkgs.mkShell {
+  name = "platformio-environment";
 
   packages = with pkgs; [
-    mpremote
-    cargo-espflash
+    platformio
     libiconv
   ];
 }
